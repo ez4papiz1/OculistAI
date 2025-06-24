4e6edb767b54379f073189ffdf89b58a444eb530
ez4papiz1 <artemkhach2002@gmail.com>
2025-06-24 14:28:17 -0700

    deployment backend/db

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DB_USER = os.getenv("DATABASE_USER", "root")
-DB_PASS = os.getenv("DATABASE_PASSWORD", "root")
-DB_HOST = os.getenv("DATABASE_HOST", "db")
-DB_NAME = os.getenv("DATABASE_DB", "main")
-
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+DB_URL = os.getenv("DATABASE_URL")
+if DB_URL:
+    SQLALCHEMY_DATABASE_URL = DB_URL
+else:
+    DB_USER = os.getenv("DATABASE_USER")
+    DB_PASS = os.getenv("DATABASE_PASSWORD")
+    DB_HOST = os.getenv("DATABASE_HOST")
+    DB_PORT = os.getenv("DATABASE_PORT")
+    DB_NAME = os.getenv("DATABASE_DB")
+    SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
